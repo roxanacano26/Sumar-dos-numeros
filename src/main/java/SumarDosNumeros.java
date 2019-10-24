25d569258513967d2b6e256474f0c3d2c245d259
Roxana Elisa Cano <roxana.cano@mercadolibre.com>
2019-10-24 09:06:12 -0300

    primer comit

---
+++ src/main/java/SumarDosNumeros.java
@@ -0,0 +1,8 @@
+public class SumarDosNumeros {
+
+    public Integer sumarDosNumeros(Integer numero1, Integer numero2){
+        Integer resultado;
+        resultado=numero1+numero2;
+        return resultado;
+    }
+}
---
+++ src/test/java/TestSumarNumeros.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSumarNumeros {
+
+    @Test
+     public void testQueDosNumerosSeSumen(){
+        SumarDosNumeros calculadora=new SumarDosNumeros();
+        Integer actual=calculadora.sumarDosNumeros(1,2);
+        Integer esperado=3;
+        Assert.assertEquals(esperado,actual);
+
+        
+
+    }
+}
